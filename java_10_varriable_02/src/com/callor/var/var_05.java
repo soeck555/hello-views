853b375f8f5fc7425ab3b5d33a4eca3273eab8e5
soeck555 <dyd2635@naver.com>
2022-03-25 09:31:38 +0900

    Var 프로젝트 2번 생성

---
+++ java_10_varriable_02/src/com/callor/var/var_05.java
@@ -0,0 +1,38 @@
+package com.callor.var;
+
+public class var_05 {
+	public static void main(String[] args) {
+		
+		int intNum1 = 0;
+		int intNum2 = 0;
+		
+		int intSum = 0;
+		
+		intNum1 = 55;
+		intNum2 = 77;
+		intSum = intNum1 + intNum2;
+		System.out.println(intNum1);
+		System.out.println(intNum2);
+		System.out.println(intSum);
+		
+		System.out.print(intNum1);
+		System.out.print(" + ");
+		System.out.print(intNum2);
+		System.out.print(" = ");
+		System.out.println(intSum);
+		
+		System.out.println(intNum1 + " + " + intNum2 + " = " + intSum);
+		
+		intNum1 = 101;
+		intNum1 = 202;
+		
+		System.out.printf("%d + $d = %d\n", 
+				intNum1, intNum2, intSum);
+		System.out.printf("%d * $d = %d\n", intNum1, intNum2, intNum1 * intNum2);
+		System.out.printf("%d - $d = %d\n", intNum1, intNum2, intNum1 - intNum2);
+		System.out.printf("%d / $d = %d\n", intNum1, intNum2, intNum1 / intNum2);
+		
+		
+		
+}
+}
